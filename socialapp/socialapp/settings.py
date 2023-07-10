12b7fe2eed1ab5ce8560449a79cb71ba24a95cb0
unknown <alfredolasupo@gmail.com>
2023-07-10 01:20:40 +0000

    profile setup

--- socialapp/socialapp/settings.py
+++ socialapp/socialapp/settings.py
@@ -127,3 +127,9 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'core/media/'
+
+MEDIA_ROOT = [
+    "core/media",
+]
